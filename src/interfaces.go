4989e12e5d3b57fc8de0142cad76e133c10abc74
Denys Volokhovskyi <rotaryden@gmail.com>
2023-01-22 08:06:53 +0200

    reworking and code style

--- src/interfaces.go
+++ src/interfaces.go
@@ -8,29 +8,35 @@ const (
 	GameStateWin
 )
 
-
-// IFloodableField interface is an abstraction of the game field, owns states of cells
-// This abstraction allows to flood-fill spaces with various logic over cells
-// (logic in our case:
-//	  we need to flood-fill all clear-free cells + cells with countrs on the closure border of the free region)
-// - IsFillable() - test if the cell is fillable (so can be filled with a given state by the fill())
-// - Fill()  - knows how to fill the cell with a new state
-// - GetWidth() int - abstract width
-// - GetHeight() int - abstract height
-type IFloodableField interface {
-	IsFillable(x, y int) bool
-	Fill(x, y int)
+// common part of interfaces (interface mixin)
+type IMeasurable interface {
+	// abstract width
 	GetWidth() int
+	// abstract height
 	GetHeight() int
 }
 
-// IField interface intended for game field consumers, such as UI
-// - GetCell() - return a pointer to Cell object by coordinates
-type IField interface {
-	IFloodableField
-	GetCell(int, int) *Cell
-	GetState() GameState
+// IFloodableField interface is an abstraction of the game field, owns states of cells
+// This abstraction decouples flood-fill algorithm and allows it to run on spaces with various logic over cells
+// (logic in the Proxx case:
+//	  we need to flood-fill all free and hole-adjacent cells tjose all are adjucent to the current free cells region being opened
+type IFloodableField interface {
+	IMeasurable
+	// test if the cell can be filled with a given state, 
+	// "has the old color" in terms of flood-fill, 
+	// - is free or hole-adjacent (so  by the fill())
+	IsFillable(x, y int) bool
+	// knows how to fill the cell with a new state
+	Fill(x, y int)
+}
 
+// IField interface intended for decoupling game field consumers, such as UI
+type IField interface {
+	IMeasurable
+	// return a pointer to Cell object by coordinates
+	GetCell(int, int) *Cell
+	// returns Game State
+	GetState() GameState
 }
 
 
--- src/utilities.go
+++ src/utilities.go
@@ -20,7 +20,7 @@ type Stack[T any] struct {
 	Size int
 }
 
-// Push() puts data on top of the stack. As stack is unlimited (list-based), 
+// Push() puts data on top of the stack. As stack is unlimited (list-based),
 // we dont need error/ok checks (at least for naive implementation)
 func (stk *Stack[T]) Push(data T) {
 	stk.lock.Lock()
@@ -35,7 +35,7 @@ func (stk *Stack[T]) Push(data T) {
 }
 
 // Pop() removes and returnes data from the top of the stack.
-// Will return ok = false in case of emptiness - 
+// Will return ok = false in case of emptiness -
 // same approach we do e.g. for maps: element, ok := myMap["somekey"]
 func (stk *Stack[T]) Pop() (T, bool) {
 	if stk.head == nil {
@@ -74,8 +74,8 @@ const (
 	FillEventNothingToFill
 )
 
-//-----------------------------------------------------------------------------------------------------------
-// Here is a non-recursive stack-based scan line algorithm 
+// -----------------------------------------------------------------------------------------------------------
+// Here is a non-recursive stack-based scan line algorithm
 // rewritten in Go from here: https://lodev.org/cgtutor/floodfill.html
 // it has been chosen because of speed, program stack safety and relative simplicity
 // - x, y int - coordinates of the seed to start from
@@ -95,28 +95,30 @@ func FloodFill(x, y int, field IFloodableField) FillEvent {
 
 	for point, ok := stack.Pop(); ok; point, ok = stack.Pop() {
 		dx := point.X
-		y := point.Y
-		for dx >= 0 && field.IsFillable(dx, y) {
+		lineY := point.Y
+
+		for dx >= 0 && field.IsFillable(dx, lineY) {
 			dx--
 		}
 		dx++
+
 		spanAbove := false
 		spanBelow := false
 
-		for dx < width && field.IsFillable(dx, y) {
-			field.Fill(dx, y)
+		for dx < width && field.IsFillable(dx, lineY) {
+			field.Fill(dx, lineY)
 
-			if !spanAbove && y > 0 && field.IsFillable(dx, (y-1)) {
-				stack.Push(Point{dx, y - 1})
+			if !spanAbove && lineY > 0 && field.IsFillable(dx, (lineY-1)) {
+				stack.Push(Point{dx, lineY - 1})
 				spanAbove = true
-			} else if spanAbove && y > 0 && !field.IsFillable(dx, (y-1)) {
+			} else if spanAbove && lineY > 0 && !field.IsFillable(dx, (lineY-1)) {
 				spanAbove = false
 			}
 
-			if !spanBelow && y < height-1 && field.IsFillable(dx, (y+1)) {
-				stack.Push(Point{dx, y + 1})
+			if !spanBelow && lineY < height-1 && field.IsFillable(dx, (lineY+1)) {
+				stack.Push(Point{dx, lineY + 1})
 				spanBelow = true
-			} else if spanBelow && y < height-1 && !field.IsFillable(dx, (y+1)) {
+			} else if spanBelow && lineY < height-1 && !field.IsFillable(dx, (lineY+1)) {
 				spanBelow = false
 			}
 			dx++
